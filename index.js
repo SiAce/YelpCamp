9cf352404f8d70b77fceb9c11cd39cc5bbc03b82
SiAce <siacespark@gmail.com>
2019-07-20 22:06:54 +0800

    Finish Basic Styling

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser');
+
+const campgrounds = [
+  { name: 'Lori', image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg' },
+  { name: 'Gala', image: 'https://www.michigan.org/sites/default/files/styles/15_6_desktop/public/camping-hero_0_0.jpg?itok=mgGs0-vw&timestamp=1520373602' },
+  { name: 'Ashito', image: 'https://www.newzealand.com/assets/Tourism-NZ/Nelson/ba40378fe9/img-1536928144-4748-13836-F53C3949-ED9E-E0DC-CF6EC0D789D9308A__FocalPointCropWzI0MCw0ODAsNTAsNTAsNzUsImpwZyIsNjUsMi41XQ.jpg' },
+];
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -9,13 +18,20 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-  const campgrounds = [
-    { name: 'Lori', image: 'https://www.reserveamerica.com/webphotos/racms/articles/images/bca19684-d902-422d-8de2-f083e77b50ff_image2_GettyImages-677064730.jpg' },
-    { name: 'Gala', image: 'https://www.michigan.org/sites/default/files/styles/15_6_desktop/public/camping-hero_0_0.jpg?itok=mgGs0-vw&timestamp=1520373602' },
-    { name: 'Ashito', image: 'https://www.newzealand.com/assets/Tourism-NZ/Nelson/ba40378fe9/img-1536928144-4748-13836-F53C3949-ED9E-E0DC-CF6EC0D789D9308A__FocalPointCropWzI0MCw0ODAsNTAsNTAsNzUsImpwZyIsNjUsMi41XQ.jpg' },
-  ];
-
   res.render('campgrounds', { campgrounds });
+});
+
+app.post('/campgrounds', (req, res) => {
+  // get data from form and add to campgrounds array
+  const { name, image } = req.body;
+  const newCampground = { name, image };
+  campgrounds.push(newCampground);
+  // redirect back to campgrounds page
+  res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 app.listen(3000, () => {
